708f6826c6b2e781e58e3a05de56d0fbcd26a7b0
Balanica Andrei <andoss1710@gmail.com>
2023-01-12 18:48:20 +0200

    Added a element

--- Project_DAW/Controllers/WeatherForecastController.cs
+++ Project_DAW/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Project_DAW.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "ceva de la mine"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "ceva"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
